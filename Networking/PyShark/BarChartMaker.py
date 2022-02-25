a942618cb8ab6ee16d9a6ca50d572f95b4aa125a
Jonas Malm <jonas@egenart.org>
2022-02-25 01:30:27 +0100

    Update BarChartMaker.py
    Changed the word "Frequency" to "Occurrences" because details matter. 
    Also, pyshark with only_summaries=True seems to have moved the protocol column to [6] instead of [4].

--- Networking/PyShark/BarChartMaker.py
+++ Networking/PyShark/BarChartMaker.py
@@ -8,13 +8,13 @@ protocolList=[]
 for packet in cap:
 	line = str(packet)
 	formattedLine= line.split(" ")
-	protocolList.append(formattedLine[4])
+	protocolList.append(formattedLine[6])
 counter = collections.Counter(protocolList)
 
 plt.style.use('ggplot')
 y_pos = np.arange(len(list(counter.keys())))
 plt.bar(y_pos,list(counter.values()),align='center',alpha=0.5,color=['b','g','r','c','m'])
 plt.xticks(y_pos,list(counter.keys()))
-plt.ylabel("Frequency")
+plt.ylabel("Occurrences")
 plt.xlabel("Protocol Name")
 plt.savefig("ProtocolGraph.png")
